e702bf19281086060d85092d9c094ef2bc6c03b5
amit7itz <amit.i@otterize.com>
2022-08-23 17:10:13 +0300

    Made client non-interactive

--- client/main.go
+++ client/main.go
@@ -6,77 +6,82 @@ import (
 	"context"
 	"fmt"
 	"github.com/samber/lo"
-	"os"
-	"strings"
 )
-
-func stringInput(message string) string {
-	fmt.Print(message)
-	var input string
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		return ""
-	}
-	return strings.TrimSpace(input)
-}
 
 func main() {
 	c := client.NewClient("http://localhost:8080/query")
 
-	demo := stringInput("Enter 1 for error types demo, or 2 for error masking demo: ")
-	if demo == "1" {
-		println(`Starting error types demo
+	// Error types demo
+	println(ErrorTypesDemoDescription)
+	firstName := "Ms"
+	lastName := "G"
+	retry := false
 
-In this demo the client is going ask for first and last name, send them as parameters to errorTypesDemo() query.
-The server is going to validate the parameters lengths, and if one of them is too short, it will return an error with the appropriate message and type.
-The client will check the returned error type, ask to type again only the bad parameter, and will try to call the server again.
-`)
-		_ = stringInput("Press enter to start\n")
+	fmt.Printf("Calling server with name \"%s %s\"...", firstName, lastName)
+	res, err := c.ErrorTypesDemo(context.Background(), firstName, lastName)
+	if err != nil {
+		println("\nServer returned an error:")
+		fmt.Printf("  - error message: %s", err)
+		errType := gqlerrors.GetGQLErrorType(err)
+		fmt.Printf("  - error type: %s\n\n", lo.Ternary(len(errType) > 0, errType, "unspecified"))
+		switch errType {
+		case gqlerrors.ErrorTypeBadFirstName:
+			fmt.Printf("First name %s is bad, will try again with \"Ariana\" instead\n", firstName)
+			firstName = "Ariana"
+			retry = true
+		case gqlerrors.ErrorTypeBadLastName:
+			fmt.Printf("Last name %s is bad, will try again with \"Grande\" instead\n", lastName)
+			lastName = "Grande"
+			retry = true
+		default:
+			println("Server did not specify error type, so the client code can't handle the error.")
+			break
+		}
+	} else {
+		fmt.Printf("\nServer returned: %s", res)
+	}
 
-		firstName := stringInput("Enter first name: ")
-		lastName := stringInput("Enter last name: ")
+	if retry {
+		fmt.Printf("Retrying to call server with new name \"%s %s\"...", firstName, lastName)
 		res, err := c.ErrorTypesDemo(context.Background(), firstName, lastName)
-		for err != nil {
+		if err != nil {
 			println("\nServer returned an error:")
 			fmt.Printf("  - error message: %s", err)
 			errType := gqlerrors.GetGQLErrorType(err)
 			fmt.Printf("  - error type: %s\n\n", lo.Ternary(len(errType) > 0, errType, "unspecified"))
-			switch errType {
-			case gqlerrors.ErrorTypeBadFirstName:
-				firstName = stringInput("Please enter another first name: ")
-			case gqlerrors.ErrorTypeBadLastName:
-				lastName = stringInput("Please enter another last name: ")
-			default:
-				println("Server did not specify error type, so the client code can't handle the error.")
-				break
-			}
-			res, err = c.ErrorTypesDemo(context.Background(), firstName, lastName)
+		} else {
+			fmt.Printf("\nServer returned: %s", res)
 		}
-		fmt.Printf("\nServer returned: %s", res)
-		println("\n\nDemo completed")
+	}
+	println("\n\nError types demo completed\n")
 
-	} else if demo == "2" {
-		println(`Starting error masking demo
+	// Error masking Demo
+	println(ErrorMaskingDemoDescription)
+	err = c.ErrorMaskingDemo(context.Background())
+	errType := gqlerrors.GetGQLErrorType(err)
+	if errType == gqlerrors.ErrorTypeInternalServerError {
+		println("Success - the server masked the internal error. server returned:")
+	} else {
+		println("Oops - the server did not mask the internal error. server returned:")
+	}
+	fmt.Printf("  - error message: %s", err)
+	fmt.Printf("  - error type: %s", lo.Ternary(len(errType) > 0, errType, "unspecified"))
+
+	println("\n\nError masking demo completed")
+}
+
+const (
+	ErrorTypesDemoDescription = `### Error types demo ###
+
+In this demo the client send first and last names to errorTypesDemo() query.
+The server is going to validate the parameters lengths, and if one of them is too short, it will return an error with the appropriate message and type.
+The client will check the returned error type, and replace the bad name, and retry.
+Starting demo...
+`
+	ErrorMaskingDemoDescription = `### Error masking demo ###
 
 In this demo the client is going to call the errorMaskingDemo() query, that causes an unexpected error in the server.
 The server should mask the internal error, and we should receive a generic internal error message, with the "InternalServerError" error type.
-`)
-		_ = stringInput("Press enter to start\n")
-
-		err := c.ErrorMaskingDemo(context.Background())
-		errType := gqlerrors.GetGQLErrorType(err)
-		if errType == gqlerrors.ErrorTypeInternalServerError {
-			println("Success - the server masked the internal error. server returned:")
-		} else {
-			println("Oops - the server did not mask the internal error. server returned:")
-		}
-		fmt.Printf("  - error message: %s", err)
-		fmt.Printf("  - error type: %s", lo.Ternary(len(errType) > 0, errType, "unspecified"))
-
-		println("\n\nDemo completed")
-
-	} else {
-		println("invalid option, exiting")
-		os.Exit(1)
-	}
-}
+Starting demo...
+`
+)
